bc6207bddc648d67f84047b9bb91c1713b52493f
Egor Falkov <egorfalkov@mail.ru>
2016-05-20 00:11:00 +0300

    add fragment map

---
+++ app/src/main/java/com/hse_miem/fb_miem/fragments/ShopCartMapTabs/ShopCartFragment.java
@@ -0,0 +1,15 @@
+package com.hse_miem.fb_miem.fragments.ShopCartMapTabs;
+
+import com.hse_miem.fb_miem.fragments.BaseFragment;
+
+/**
+ * Created by Egor on 20/05/16.
+ */
+public class ShopCartFragment extends BaseFragment {
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//
+//    }
+}
---
+++ app/src/main/java/com/hse_miem/fb_miem/model/Pin.java
@@ -0,0 +1,11 @@
+package com.hse_miem.fb_miem.model;
+
+/**
+ * Created by Egor on 20/05/16.
+ */
+public class Pin {
+
+//    @Getter @Setter
+
+
+}
